5a71f3e34776342d044c56b0d5e2ff1dc0fb3f34
viniciussilvabarros05 <viniciussilvabarros05@hotmail.com>
2024-02-26 16:59:46 -0300

    chore: add footer in layout

---
+++ src/app/_components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = ()=>{
+    return(
+        <div className="w-full bg-secondary py-4 px-5">
+
+            <p className="text-gray-400 text-xs font-bold opacity-75">
+             © 2023 Copyright FSW Barber
+            </p>
+        </div>
+
+    )
+}
+
+export default Footer
